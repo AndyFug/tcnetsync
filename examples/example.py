1a72a7a7c637cadf363b0349de1079b5c2793e1d
AndyFug <66973822+AndyFug@users.noreply.github.com>
2020-12-07 22:40:57 +0000

    Updates to example.py to provide different logging combinations.
    Signed-off-by: AndyFug <66973822+AndyFug@users.noreply.github.com>

--- examples/example.py
+++ examples/example.py
@@ -2,12 +2,17 @@ import logging
 
 from tcnetsync import tc_server, tc_client, tc_reader
 
-# logging.basicConfig(level=logging.DEBUG)
+# Set root logger
+logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
 
 # Setup server, client and reader
 server = tc_server.MTCServer()
 client = tc_client.TcSyncClient()
 reader = tc_reader.TcReader(client, freeroll=3)
+
+# server.logger.setLevel(logging.DEBUG)
+# client.logger.setLevel(logging.DEBUG)
 
 server.start()
 client.start()
